7182f57b76e3c2396ea476de88f4a6fdf27cbf77
thomas <coreisdontgu@gmail.com>
2020-03-21 22:49:53 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import List from "./List.js";
+import Input from "./Input.js";
+import Parent from "./ParentChildProps/Parents.js";
 
-ReactDOM.render(<List />, document.getElementById("root"));
+// ReactDOM.render(<List />, document.getElementById("root"));
+//不太了解為什麼沒有辦法一起show出來
+ReactDOM.render(<Input />, document.getElementById("input"));
+ReactDOM.render(<Parent />, document.getElementById("root"));

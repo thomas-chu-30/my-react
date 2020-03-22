78934edbb8ffe7eb23bee2fcecaa97d7d331a20a
thomas <coreisdontgu@gmail.com>
2020-03-22 14:43:38 +0800

    update

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h3>hellow World</h3>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
